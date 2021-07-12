6fc930b907819fcfba3f2b5cc5c81c05a7043555
korov <korova2321@gmail.com>
2021-07-13 00:27:44 +0300

    Created extension method for adding generated validators

---
+++ MediatR.ValidationGenerator/DiExtensions.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MediatR.ValidationGenerator.Gen;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MediatR.ValidationGenerator
+{
+    public static class DiExtensions
+    {
+        public static IServiceCollection AddGeneratedValidators(
+            this IServiceCollection services,
+            IEnumerable<Assembly> assemblies,
+            ServiceLifetime lifetime = ServiceLifetime.Transient,
+            Func<AssemblyScanner.AssemblyScanResult, bool> filter = null
+            )
+        {
+            services.Add(new ServiceDescriptor(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>), lifetime));
+
+            assemblies = assemblies.Union(new List<Assembly> { typeof(SourceGenerator).Assembly });
+
+            services.AddValidatorsFromAssemblies(assemblies, lifetime, filter);
+
+            return services;
+        }
+    }
+}
