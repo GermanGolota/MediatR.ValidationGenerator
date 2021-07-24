331afe816cccc6e9b4e326c863dd776979612f4c
korov <korova2321@gmail.com>
2021-07-24 18:00:59 +0300

    Updated validator generator to define rules in the constructor

--- MediatR.ValidationGenerator.Gen/RequestValidatorCreator.cs
+++ MediatR.ValidationGenerator.Gen/RequestValidatorCreator.cs
@@ -20,40 +20,31 @@ namespace MediatR.ValidationGenerator.Gen
                      .WithNamespace(VALIDATORS_NAMESPACE)
                      .UsingNamespace("FluentValidation")
                      .Implementing($"AbstractValidator<{requestClassName}>")
-                     .WithMethod((method) =>
+                     .WithConstructor(ctor =>
                      {
-                         return method
-                             .AsOverride()
-                             //.AsAsync()
-                             .WithReturnType("Task<ValidationResult>")
-                             .WithName("ValidateAsync")
-                             .WithParameter($"ValidationContext<{requestClassName}>", "context")
-                             .WithParameter("CancellationToken", "cancellation", "default")
-                             .WithBody((body) =>
+                         return ctor.WithBody((body) =>
+                         {
+                             foreach (var entry in model.PropertyToSupportedAttributes)
                              {
-                                 foreach (var entry in model.PropertyToSupportedAttributes)
+                                 var prop = entry.Key;
+                                 var attributes = entry.Value;
+                                 body.AppendLine($"RuleFor(x => x.{prop.Identifier})", endLine: false);
+
+                                 List<string> rules = attributes
+                                                .Select(attribute => AttributeService.CreateRulesForAttribute(attribute))
+                                                .Flatten()
+                                                .ToList();
+
+                                 for (int i = 0; i < rules.Count; i++)
                                  {
-                                     var prop = entry.Key;
-                                     var attributes = entry.Value;
-                                     body.AppendLine($"RuleFor(x => x.{prop.Identifier})", endLine: false);
-
-                                     List<string> rules = attributes
-                                                    .Select(attribute => AttributeService.CreateRulesForAttribute(attribute))
-                                                    .Flatten()
-                                                    .ToList();
-
-                                     for (int i = 0; i < rules.Count; i++)
-                                     {
-                                         bool isLastRule = i == rules.Count - 1;
-                                         var rule = rules[i];
-                                         body.AppendLine(rule, 1, isLastRule);
-                                     }
+                                     bool isLastRule = i == rules.Count - 1;
+                                     var rule = rules[i];
+                                     body.AppendLine(rule, 1, isLastRule);
                                  }
+                             }
 
-                                 body.AppendLine("return base.ValidateAsync(context, cancellation)");
-
-                                 return body;
-                             });
+                             return body;
+                         });
                      });
 
             return classBuilder.Build();
