ecd5cc55fcb00c6e4f6885c4004bd0ecd13845a4
korov <korova2321@gmail.com>
2021-11-29 07:35:58 +0200

    Update readme

--- Example/ExampleApp/Requests/DuplicateRequest.cs
+++ Example/ExampleApp/Requests/DuplicateRequest.cs
@@ -6,6 +6,7 @@ namespace ExampleApp.Requests
     public class DuplicateRequest : IRequest<string>
     {
         [Required]
+        [RegularExpression("[A-Z,a-z,0-9,-]")]
         public string Text { get; set; }
     }
 }
